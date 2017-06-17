5ef9f7bf90a2a5ba3aff1b5c16b664e5772f3365
Steven Morkert <Steven.Morkert@gmail.com>
2017-06-16 20:40:30 -0700

    Update i2s.h

--- i2s.h
+++ i2s.h
@@ -52,6 +52,8 @@
 #define I2S_RX_PIN_PATTERN_5   0x50         // Receive pins 27, 29, 13, 28 (MCLK on 28)
 #define I2S_RX_PIN_PATTERN_6   0x60         // Receive pins 27, 29, 13, 11 (MCLK on 11)
 
+#define I2S_PIN_PATTERN       I2S_TX_PIN_PATTERN_2 + I2S_RX_PIN_PATTERN_6
+
 
 // DMA buffer size (in samples).
 // Using ping-pong DMA, this determines your latency.
