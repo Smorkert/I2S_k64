8ed4eb00ab711d979de14b567c399d6a83e1318f
Steven Morkert <Steven.Morkert@gmail.com>
2017-06-17 10:53:45 -0700

    Update i2s_tx_dma.ino

--- examples/i2s_tx_dma/i2s_tx_dma.ino
+++ examples/i2s_tx_dma/i2s_tx_dma.ino
@@ -35,6 +35,8 @@ Tx pin 11: I2S0_MCLK     <--> MCLK
 #include <i2s.h>
 #define CLOCK_TYPE          (I2S_CLOCK_48K_INTERNAL)
 #define clock_per_sec       48000
+#define CPU_CLK             96  //System CLK in MHz
+#define I2S_PIN_PATTERN     I2S_TX_PIN_PATTERN_2  //TX Pin Pattern, see <i2s.h>
 
 // audio data
 int16_t audf, audx, audy, audd;
