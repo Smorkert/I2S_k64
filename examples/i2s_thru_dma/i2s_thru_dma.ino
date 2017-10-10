facd692fe455524c6019edc9bde92e7c38997855
Steven Morkert <Steven.Morkert@gmail.com>
2017-10-09 19:57:01 -0700

    Update i2s_thru_dma.ino

--- examples/i2s_thru_dma/i2s_thru_dma.ino
+++ examples/i2s_thru_dma/i2s_thru_dma.ino
@@ -1,9 +1,9 @@
 /*
-  I2S w/ DMA digital audio demonstrator for Teensy 3.5
-  The current configuration is set up fo sample rate = 96k, 32 bit values, MCLK = 24.576MHz
+  I2S w/ DMA for Teensy 3.x in use with WM8731 Codec (Any codec should work though)
+  The current configuration is set up for sample rate = 88.2kHz, 32 bit values, MCLK_EXT = 11.2896
   See header file for specifics
-  TESTED and WORKING (only transmit tested)
-  PINS: (pin pattern 2)
+  TESTED and WORKING
+  PINS: (I2S_CODEC_PIN_PATTERN)
   Tx pin 3 : I2S0_TXD0     <--> TX Data
   Rx pin 13: I2S0_RXD0     <--> RX Data
   Tx pin 4 : I2S0_TX_FS    <--> LRCLK
@@ -14,22 +14,25 @@
 
 //I2S digital audio
 #include "i2s.h"
-#define CLOCK_TYPE             (I2S_CLOCK_INTERNAL)  // Sample Rate
+#include <Wire.h>
+#include "AudioCodec.h"
+#define CLOCK_TYPE             (I2S_CLOCK_EXTERNAL)  // Sample Rate
+#define SAMPLE_RATE 88         //88 or 44, change BCLK_DIV in i2s.h (0: 88.2KHz-32-bit, 1:88.2KHz-16-bit, 1: 44.1KHz-32-bit, 3: 44.1KHz-16-bit)
 
-// Circular buffer for audio samples, interleaved left & right channel
+// Circular buffer for audio samples, interleaved left & right channel, change intXX_t for 32-bit or 16-bit audio
 const uint16_t buffersize = DMA_BUFFER_SIZE; // must be a multiple of DMA_BUFFER_SIZE
-volatile int16_t buffer[buffersize];
+volatile int32_t Buffer[buffersize];
 uint16_t nTX = 0;
 uint16_t nRX = 0;
 
 /* --------------------- DMA I2S Receive, we get callback to read 2 words from the FIFO ----- */
 
-void dma_rx_callback( int16_t *pBuf, uint16_t len )
+void dma_rx_callback( int32_t *pBuf, uint16_t len )
 {
   while( len>0 )
   {
-    buffer[nRX++] = *pBuf++;
-    buffer[nRX++] = *pBuf++;
+    Buffer[nRX++] = *pBuf++;
+    Buffer[nRX++] = *pBuf++;
     len--;
     len--;
   }
@@ -38,12 +41,12 @@ void dma_rx_callback( int16_t *pBuf, uint16_t len )
 
 /* --------------------- DMA I2S Transmit, we get callback to put 2 words into the FIFO ----- */
 
-void dma_tx_callback( int16_t *pBuf, uint16_t len )
+void dma_tx_callback( int32_t *pBuf, uint16_t len )
 {
   while( len>0 )
-  {
-    *pBuf++ = buffer[nTX++];
-    *pBuf++ = buffer[nTX++];
+  { 
+    *pBuf++ = Buffer[nTX++];
+    *pBuf++ = Buffer[nTX++];
     len--;
     len--;
   }
@@ -54,16 +57,17 @@ void dma_tx_callback( int16_t *pBuf, uint16_t len )
 
 void setup()
 {
+  AudioCodec_init(); // setup codec registers
   Serial.println( "Initializing" );
   
-  delay(2000); 
+  delay(200); 
   Serial.println( "Initializing." );
   
   //configure Audio Codec
   
-  delay(1000);
-  I2SRx0.begin( CLOCK_TYPE, dma_rx_callback );
-  I2STx0.begin( CLOCK_TYPE, dma_tx_callback );
+  delay(100);
+  I2SRx0.begin(CLOCK_TYPE, dma_rx_callback);
+  I2STx0.begin(CLOCK_TYPE, dma_tx_callback);
   
   // Before starting tx/rx, set the buffer pointers
   nRX = 0;
